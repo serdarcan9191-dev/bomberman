49afff58966f3ef2865c95a45ebe921c5e524521
serdar6061 <serdar.can@egebis.com.tr>
2025-12-19 01:55:41 +0300

    4

--- backend/sql/init_rooms.sql
+++
@@ -1,9 +0,0 @@
--- Rooms Table Initialization Script
--- Bu script'i çalıştırarak rooms tablolarını oluşturun
-
--- Önce schema'yı çalıştırın
-\i schema_rooms.sql
-
--- Veya doğrudan bu dosyayı çalıştırın:
--- psql -h <host> -U <user> -d <database> -f init_rooms.sql
-
